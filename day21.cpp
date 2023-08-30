a74fddbaa7329b0e9f251ab68c57abe68c9a72a9
Kaushiki Mishra <112900571+kaushiki2003@users.noreply.github.com>
2023-08-30 16:27:08 +0530

    Add files via upload

---
+++ day21.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=9;
+    cout<<"hello world"<< sum;
+    return 0;
+}
---
+++ day22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+  
+// Standard Namespace
+using namespace std;
+  
+// Main Function
+ int main()
+{
+  
+    // Body of the Function
+  
+    // Declaration of Variable
+    int num1 = 24;
+    int num2 = 34;
+  
+    int result = num1 + num2;
+  
+    // Output
+    cout << result << endl;
+  
+    // Return Statement
+    return 0;
+}
